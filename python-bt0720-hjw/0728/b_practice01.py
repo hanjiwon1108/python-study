2f9e7a67f6497ee0fbe2a1b21867a054891d3ab6
hanjiwon1108 <jwjwhan1108@gmail.com>
2023-07-31 11:49:29 +0900

    20230731 python

---
+++ python-bt0720-hjw/0728/b_practice01.py
@@ -0,0 +1,14 @@
+
+print("apple banana apple cherry apple".count("apple"))
+
+print("apple banana apple cherry apple".find("banana"))
+
+print("apple banana apple cherry apple".title())
+
+print(', '.join(['apple', 'banana', 'cherry']))
+
+print("apple,banana,cherry".split(', '))
+
+print("Hello, world".replace("world", "python"))
+
+print(" Hello, world! ".strip())
